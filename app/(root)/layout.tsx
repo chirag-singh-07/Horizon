a38d82d16b92cfb0b176718df65b6aa6a5585259
Chirag Singh <chiraaajsingh7600@gmail.com>
2025-04-19 18:00:29 +0530

    sign up and sign in ui  is completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import MoblieNav from "@/components/MoblieNav";
 import SideBar from "@/components/SideBar";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
@@ -8,22 +11,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const loggedIn = { firstName: "Chirag", lastName: "Singh" };
+  const pathname = usePathname();
+
+  const isAuthPage =
+    pathname === "/login" || pathname === "/sign-in" || pathname === "/sign-up";
 
   return (
     <main className="flex h-screen w-full font-inter">
-      <SideBar user={loggedIn} />
+      {!isAuthPage && <SideBar user={loggedIn} />}
       <div className="flex size-full flex-col">
-        <div className="root-layout">
-          <Image
-            src={"/icons/logo.svg"}
-            alt="menu logo"
-            width={30}
-            height={30}
-          />
-          <div>
-            <MoblieNav user={loggedIn} />
+        {!isAuthPage && (
+          <div className="root-layout">
+            <Image
+              src={"/icons/logo.svg"}
+              alt="menu logo"
+              width={30}
+              height={30}
+            />
+            <div>
+              <MoblieNav user={loggedIn} />
+            </div>
           </div>
-        </div>
+        )}
         {children}
       </div>
     </main>
